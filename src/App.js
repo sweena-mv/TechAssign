6cf9212cab3c59b62f9f2b6d13144ee571e662ae
sweena-mv <86863301+sweena-mv@users.noreply.github.com>
2022-08-22 19:53:01 +0400

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -9,25 +9,13 @@ import Navbar from './components/inc/Navbar';
 
 function App() {
 
-  const [readMore,setReadMore]=useState(false);
-  const extraContent=<div>
-      <p className="extra-content">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, consectetur neque ab 
-        porro quasi culpa nulla rerum quis minus voluptatibus sed hic ad quo sint, libero 
-        commodi officia aliquam! Maxime.
-      </p>
-  </div>
-  const linkName=readMore?'Read Less << ':'Read More >> '
 
-  return (
+ return (
     <Router>
     <Navbar />
 <Switch>
 <Route path="/" exact  component={Home} />
-<div className="App">
-      <a className="read-more-link" onClick={()=>{setReadMore(!readMore)}}><h2>{linkName}</h2></a>
-      {readMore && extraContent}
-    </div>
+
 </Switch>
 
     </Router>
